1338df7e5a56897351205c4a94da4d0ebbcf1c93
Welingson E. Santos <welingson12@gmail.com>
2020-04-28 14:40:58 -0300

    Alteração de layout e estilização da view home

--- themes/web/home.php
+++ themes/web/home.php
@@ -6,17 +6,69 @@
         <p id="banner-description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut, modi repellat. Nostrum nobis laborum impedit incidunt tenetur asperiores quisquam voluptatum? Quis in enim praesentium rerum sapiente eaque necessitatibus voluptas quisquam!</p>
     </article>
 </section>
+<header id="header-description">
+    <h2 id="header-title">Feira Online </h2>
+    <p id="header-description-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad veritatis officia distinctio fugiat cum. Incidunt, perferendis quas excepturi magnam recusandae perspiciatis eveniet minus. Temporibus, fuga sunt. Tempore atque dolorem non?</p>
+</header>
 <section id="section-about-list">
     <div>
-        <img src="" alt="">
+        <i class="fas fa-chart-line"></i>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
     </div>
     <div>
-        <img src="" alt="">
+        <i class="fas fa-play-circle"></i>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
     </div>
     <div>
-        <img src="" alt="">
+        <i class="fas fa-address-card"></i>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
     </div>
 </section>
+<article id="home-live-preview">
+    <div id="iframe-more">
+        <h2 id="iframe-more-title">Algumas feiras ao vivo agora</h2>
+        <a id="iframe-more-button" href="" class="btn">Quero ver mais<i class="fas fa-share-square ml-2"></i></a>
+    </div>
+    <hr class="mb-4">
+    <div class="container">
+        <div id="iframe">
+            <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FLoLMinerva%2Fvideos%2F223461419071657%2F&show_text=0&width=560" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
+            <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FFOX2Now%2Fvideos%2F3693420197394752%2F&show_text=0&width=560" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
+            <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fnewshour%2Fvideos%2F518502122166779%2F&show_text=0&width=560" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 col-md-6 col-lg-6">
+                <h2 id="home-optin-title" class="font-weight-bold">Cadastre-se agora e conheça</h2>
+                <p id="">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad veritatis officia
+                    distinctio fugiat cum. Incidunt, perferendis quas excepturi magnam recusandae perspiciatis eveniet minus.
+                    Temporibus, fuga sunt. Tempore atque dolorem non?
+                </p>
+                <p id="">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad veritatis officia
+                    distinctio fugiat cum. Incidunt, perferendis quas excepturi magnam recusandae perspiciatis eveniet minus.
+                    Temporibus, fuga sunt. Tempore atque dolorem non?
+                </p>
+            </div>
+            <div class="co-sm-12 col-md-6 col-lg-6">
+                <form id="home-form-optin">
+                    <i class="fas fa-check-square"></i>
+                    <h4>Cadastre-se gratuitamente:</h4>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Primeiro nome:">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Ultimo nome:">
+                    </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control" placeholder="Seu melhor e-mail:">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" placeholder="Senha de acesso">
+                    </div>
+                    <button type="submit" class="btn btn-primary text-center">Quero me cadastrar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</article>
